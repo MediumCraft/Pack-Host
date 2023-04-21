de587d718d431ebdf553180eac7bf3bc12cb619e
LeeHansHinLun <79551623+LeeHansHinLun@users.noreply.github.com>
2023-04-20 21:01:38 -0500

    Added pack-host.py

---
+++ pack-host.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2023 MediumCraft
+# MediumCraft Protective License
+# If the license had not came with the Software, please see https://github.com/MediumCraft/License
+
+import http.server
+import socketserver
+import os
+
+PORT = 8090 # The port of the http server
+FILE_NAME = "pack.zip" # The only file accessible by the client
+
+print("MediumCraft Resource Pack Server")
+print("Starting server...")
+
+class RestrictedHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    
+    def translate_path(self, path):
+        # Override base class method to restrict access to files outside of pack.zip
+        full_path = http.server.SimpleHTTPRequestHandler.translate_path(self, path)
+        if not full_path.endswith('/' + FILE_NAME):
+            self.send_error(403, 'Forbidden')
+            return "403 Forbidden"
+        return full_path
+
+Handler = RestrictedHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}/")
+    print("CTRL+C to stop the server")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt: Stopping server..")
+    httpd.server_close()
+    print(f"Server stopped.")
